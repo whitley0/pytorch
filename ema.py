e9d9a5e80601c36a508d2612e081c53e1ad991a9
CAOANQI <38660936+whitley0@users.noreply.github.com>
2022-08-16 11:55:43 +0800

    Update and rename ztest.py to ema.py

---
+++ ema.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+'''
+author:qingxingmo
+datetime: 2022/8/14 21:47
+'''
+
+class EMA():
+    def __init__(self, beta=0.9999):
+        super().__init__()
+        self.beta = beta
+
+    def update_model_average(self, ma_model, current_model):
+        for current_params, ma_params in zip(current_model.parameters(), ma_model.parameters()):
+            old_weight, up_weight = ma_params.data, current_params.data
+            ma_params.data = self.update_average(old_weight, up_weight)
+
+    def update_average(self, old, new):
+        if old is None:
+            return new
+        return old * self.beta + (1 - self.beta) * new
